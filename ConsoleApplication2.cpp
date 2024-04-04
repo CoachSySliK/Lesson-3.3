1f370999063f93c394cc78b5d8aecf3dd94d0098
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-04 13:15:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string userName;
+    string race;
+
+    cout << "Введите имя вашего персонажа: ";
+    cin >> userName;
+    cout << "Введите название вашей расы: ";
+    cin >> race;
+    cout << "В далёкой-далёкой галактике родился " << race << " по имени " << userName << ".\nДа прибудет с тобой сила, " << userName << "!\n";
+
+    return 0;
+}
